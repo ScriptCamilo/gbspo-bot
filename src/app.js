a8e1a7d2034ec0a0891ab75362dbd54dc62a0610
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-01-18 18:56:26 -0300

    Main code updated

--- src/app.js
+++ src/app.js
@@ -1,35 +1,61 @@
 require('dotenv').config()
+
 const { Telegraf } = require("telegraf");
 const fs = require('fs');
+const schedule = require("node-schedule")
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Erro caso não seja a pessoa certa acessando o BOT
 bot.catch((err, ctx) => {
-  return ctx.replyWithDocument({
-    source: fs.readFileSync('./assets/stickers/denied.webp'),
+  console.log(`Ooops, encoutered an error for ${ctx.updateType}`, err)
+  ctx.replyWithDocument({
+    source: fs.readFileSync('./assets/stickers-bot/denied.webp'),
     filename: 'denied.webp'
   })
+  return ctx.leaveChat();
 })
 
+// O start presente em todos os BOTS, mas também filtrando se o ID corresponde ao da pessoa certa
 bot.start((ctx) => {
   if (ctx.chat.id !== Number(process.env.ADMIN_ID)) {
     throw new Error('Authentication error')
   }
-  return ctx.replyWithDocument({
-    source: fs.readFileSync('./assets/stickers/granted.webp'),
+  ctx.replyWithDocument({
+    source: fs.readFileSync('./assets/stickers-bot/granted.webp'),
     filename: 'granted.webp'
+  })
+  
+  // A partir do comando start o bot criará um schedule de mensagens que se repetirá em um horário específico
+  schedule.scheduleJob('* 35 * * * *', () => {
+    console.log("Foi, amém");
+    // Estou eviando um sticker diretamente para um chat específico
+    ctx.telegram.sendDocument(process.env.ADMIN_ID, {
+      source: fs.readFileSync('./assets/stickers-bot/pill.webp'),
+      filename: 'pill.webp'
+    })
+    // schedule.scheduleJob('/6 * * * *', () => {
+    //   ctx.telegram.sendDocument(process.env.ADMIN_ID, {
+    //     source: fs.readFileSync('./assets/stickers-bot/alert.webp'),
+    //     filename: 'alert.webp'
+    //   })
+    // })
   })
 })
 
+
+// Command para quando ela tiver tomando o remédio avisar ao BOT
 bot.command('tomei', (ctx, next) => {
   if (ctx.chat.id !== Number(process.env.ADMIN_ID)) {
     throw new Error('Authentication error')
   }
   return ctx.replyWithDocument({
-    source: fs.readFileSync('./assets/stickers/baby.webp'),
+    source: fs.readFileSync('./assets/stickers-bot/baby.webp'),
     filename: 'baby.webp'
   })
 })
+
+// Preciso criar também o fato de não ter tomado o remédio em determinado tempo
 
 bot.launch();
 
