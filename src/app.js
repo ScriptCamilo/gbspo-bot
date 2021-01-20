10b31a0ad1dfbc5059b2e52c25c4ba1c9dbebfa9
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-01-20 08:22:59 -0300

    function to organize schedule

--- src/app.js
+++ src/app.js
@@ -12,6 +12,38 @@ let cautionScheduleOn = false;
 // Ao mesmo tempo que também quero fazer um outro comando dá a condição de parada do caution schedule 
 
 // CRIAR FUNÇÃO PARA OS SCHEDULES E DEIXAR O CÓDIGO MAIS LIMPO!!!!
+function createSchedule() {
+  const pillSchedule = schedule.scheduleJob('0 */5 * * * *', () => {
+    // Estou eviando um sticker diretamente para um chat específico
+    ctx.telegram.sendDocument(process.env.OWNER_ID, {
+      source: fs.readFileSync('./assets/stickers-bot/pill.webp'),
+      filename: 'pill.webp'
+    })
+    
+    // Criando um schedule para continuar alertando ao usuário caso ainda não tenha tomado o remédio
+    let count = 0; // Para poder alternar entre o nível de caution para o usuário
+    cautionScheduleOn = true;
+    const cautionSchedule = schedule.scheduleJob('0 */1 * * * *', () => {
+      // Condição de parada para o schedule continuar avisando ao usuário
+      if (!cautionScheduleOn) {
+        console.log("Cancelado.");
+        return cautionSchedule.cancel();
+      }
+      // Processo entre a troca de níveis para o caution
+      if (count++ < 3) {
+        ctx.telegram.sendDocument(process.env.OWNER_ID, {
+          source: fs.readFileSync('./assets/stickers-bot/alert.webp'),
+          filename: 'alert.webp'
+        })
+      } else {
+        ctx.telegram.sendDocument(process.env.OWNER_ID, {
+          source: fs.readFileSync('./assets/stickers-bot/danger.webp'),
+          filename: 'danger.webp'
+        })
+      }
+    })
+  })
+}
 
 // Erro caso não seja a pessoa certa acessando o BOT
 bot.catch((err, ctx) => {
@@ -31,42 +63,12 @@ bot.start((ctx) => {
     source: fs.readFileSync('./assets/stickers-bot/granted.webp'),
     filename: 'granted.webp'
   })
-
   
   // A partir do comando start o bot criará um schedule de mensagens que se repetirá em um horário específico
   // Estou evitando também que vários comandos start crie vários schedules
   if (!pillScheduleOn) {
     pillScheduleOn = true;
-    const pillSchedule = schedule.scheduleJob('0 */5 * * * *', () => {
-      // Estou eviando um sticker diretamente para um chat específico
-      ctx.telegram.sendDocument(process.env.OWNER_ID, {
-        source: fs.readFileSync('./assets/stickers-bot/pill.webp'),
-        filename: 'pill.webp'
-      })
-      
-      // Criando um schedule para continuar alertando ao usuário caso ainda não tenha tomado o remédio
-      let count = 0; // Para poder alternar entre o nível de caution para o usuário
-      cautionScheduleOn = true;
-      const cautionSchedule = schedule.scheduleJob('0 */1 * * * *', () => {
-        // Condição de parada para o schedule continuar avisando ao usuário
-        if (!cautionScheduleOn) {
-          console.log("Cancelado.");
-          return cautionSchedule.cancel();
-        }
-        // Processo entre a troca de níveis para o caution
-        if (count++ < 3) {
-          ctx.telegram.sendDocument(process.env.OWNER_ID, {
-            source: fs.readFileSync('./assets/stickers-bot/alert.webp'),
-            filename: 'alert.webp'
-          })
-        } else {
-          ctx.telegram.sendDocument(process.env.OWNER_ID, {
-            source: fs.readFileSync('./assets/stickers-bot/danger.webp'),
-            filename: 'danger.webp'
-          })
-        }
-      })
-    })
+    createSchedule()
   }
 })
 
